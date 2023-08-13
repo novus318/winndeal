208a4299bd7e32fd0e25536d56b32868cb2c204d
novus318 <nizamudheen318@gmail.com>
2023-08-13 11:27:44 +0530

    load gif

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import Home from './Pages/Home';
 import ProductService from './Pages/ProductService';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import Layout from './Components/Layout';
 import { Toaster } from 'react-hot-toast';
 import 'react-chatbot-kit/build/main.css'
+import Load from './Components/Load';
 
 function App() {
- 
+ const [loading, setloading] = useState(true)
+ useEffect(() => {
+  setloading(true)
+  setTimeout(()=>{
+    setloading(false)
+  },1700)
+}, [])
 useEffect(() => {
   const disableContextMenu = (event) => {
     event.preventDefault();
@@ -29,7 +36,7 @@ useEffect(() => {
   <Layout>
   <Router>
    <Routes> 
-  <Route path="/" element={<Home />} />
+  <Route path="/" element={loading? <Load/> : <Home setloading={setloading}/>} />
   <Route path="/about" element={<About />} />
   <Route path="/product-services" element={<ProductService />} />
   <Route path="/contact" element={<Contact/>} />
